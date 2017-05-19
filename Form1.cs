c1b5b89cd09c6473fc5c8064e44e4537f15a8d15
Galilea-LM <glopez1@ucol.mx>
2017-05-19 02:08:15 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDListaSimple
+{
+    public partial class CRUD : Form
+    {
+        public CRUD()
+        {
+            InitializeComponent();
+            inventario = new Inventario();
+        }
+        Inventario inventario;
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt16(txtCodigo.Text);
+            string nombre = txtNombre.Text;
+            double costo = Convert.ToDouble(txtCosto.Text);
+            int cantidad = Convert.ToInt32(txtCantidad.Text);
+            Producto producto = new Producto(codigo,nombre,costo,cantidad);
+            inventario.Insertar(producto);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt16(txtCodigo.Text);
+            if (inventario.Buscar(codigo) != null)
+            {
+                txtInventario.Text = inventario.Buscar(codigo).NombreProducto;
+            }
+            else
+            {
+                txtInventario.Clear();
+            }
+            
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt32(txtCodigo.Text);
+            inventario.Eliminar(codigo);
+        }
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+            txtInventario.Text = inventario.Datos();
+        }
+    }
+}
---
+++ Inventario.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDListaSimple
+{
+    class Inventario
+    {
+        Producto inicio;
+        Producto tem;
+        public Inventario()
+        {
+            inicio = null;
+        }
+        public void Insertar(Producto pNuevo)
+        {
+            if (inicio == null)
+            {
+                inicio = pNuevo;
+            }
+            else
+            {
+                pNuevo = inicio;
+                while (tem.siguiente == null)
+                {
+
+                    tem = tem.siguiente;
+                    tem.siguiente = pNuevo;
+                }
+            }
+
+        }
+
+        public string Datos()
+        {
+            String datos = "";
+            Producto t = inicio;
+            while (t != null)
+            {
+                datos += t.ToString();
+                t = t.siguiente;
+            }
+            return datos;
+        }
+
+
+        public void Eliminar(int codigoProducto)
+        {
+            tem = inicio;
+            if (tem.CodigoProducto == codigoProducto)
+            {
+                inicio = tem.siguiente;
+            }
+            else
+            {
+                while (tem.siguiente != null)
+                {
+                    if (tem.siguiente.CodigoProducto == codigoProducto)
+                    {
+                        if (tem.siguiente.siguiente == null)
+                        {
+                            tem.siguiente = null;
+                        }
+                        else
+                        {
+                            tem.siguiente = tem.siguiente.siguiente;
+                        }
+                    }
+                    if (tem.siguiente != null)
+                    {
+                        tem = tem.siguiente;
+                    }
+                }
+            }
+        }
+
+        public Producto Buscar(int codigo)
+        {
+            tem = inicio;
+            Producto producto = null;
+            while (tem != null)
+            {
+                if (tem.CodigoProducto == codigo)
+                {
+                    producto = tem;
+                }
+                tem = tem.siguiente;
+            }
+            return producto;
+        }
+
+    }
+}
